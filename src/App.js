d32259c7e803ba2642b0598cfed8b2e40eea7c03
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-04-02 06:50:31 +0530

    Basic UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [feedback, setFeedback] = useState([
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel velit mauris. Quisque vel dolor ac magna euismod hendrerit. Donec tempor, nibh nec lacinia bibendum, purus lorem fermentum turpis, ac molestie dolor magna et lectus.`,
+    `Fusce lacinia, sapien ac convallis commodo, quam erat semper dui, quis rutrum velit tortor in dui. Fusce aliquet euismod justo non commodo. Sed laoreet ante id enim rhoncus consequat. Donec ut leo eget nisi suscipit ultricies.`,
+  ]);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Add code to analyze the text and update the feedback state
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-700 flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-purple-300 mb-8">Personal Writing Assistant ✨</h1>
+      <div className="max-w-3xl w-full bg-purple-300 rounded-lg shadow-lg p-8">
+        <form className="mb-6" onSubmit={handleFormSubmit}>
+          <label htmlFor="text" className="block text-lg font-medium mb-2">
+            Write your text here:
+          </label>
+          <textarea
+            id="text"
+            name="text"
+            rows="5"
+            className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm mb-4 p-2 focus:outline-none"
+            placeholder="Start typing here..."
+          ></textarea>
+
+          <button
+            type="submit"
+            className="bg-purple-400 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+          >
+            Analyze
+          </button>
+        </form>
+        <div>
+          <h2 className="text-lg font-bold text-gray-800 mb-4">Feedback:</h2>
+          <div className="bg-gray-200 p-4 rounded-lg">
+            {feedback.map((paragraph, index) => (
+              <p key={index} className="text-gray-800 mb-2">
+                {paragraph}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
